8113be9af1c896a099342d0cbcbcf5bab752f5b5
Augusztinovics Janos <lancing0@gmail.com>
2022-07-09 07:32:07 +0100

    race skill sets

--- resources/js/store/modules/magus/magusRaces.js
+++ resources/js/store/modules/magus/magusRaces.js
@@ -22,7 +22,22 @@
             forbittenClasses: [],
             CE: 0,
             specials: [],
-            skills: [],
+            skills: {
+                af: [],
+                mf: [],
+                precent: {
+                    maszas: 0,
+                    eses: 0,
+                    ugras: 0,
+                    lopakodas: 0,
+                    rejtozes: 0,
+                    koteltanc: 0,
+                    zsebmetszes: 0,
+                    csabdafelfedezes: 0,
+                    zarnyitas: 0,
+                    titkosajto: 0,
+                },
+            },
             description: ['Az emberek Ynev legelterjetebb népe. Nem rendelkeznek semilyen különleges képességgel, a kasztok és leirások átlaga rájuk vonatkozik. Átlag életkoruk ugy 60-70 év.']
         },
         {
@@ -40,23 +55,22 @@
             forbittenClasses: ['LOVAG', 'TOLVAJ', 'PAP', 'PAPLOVAG', 'HARCMUVESZ', 'KARDMUVESZ', 'BOSZORKANY', 'BOSZORKANYMESTER', 'TUZVARAZSLO'],
             CE: 20,
             specials: ['Kétszer jobb hallás', 'Két és félszer jobb látás', 'Infralátás 50 méterig', 'Nekromancia érzékenység -8'],
-            skills: [
-                {
-                    name: 'Lovaglás',
-                    level: 'MF',
-                    kp: 0,
+            skills: {
+                af: [],
+                mf: ['LOVAGLAS', 'IDOMITAS', 'ERDOJARAS'],
+                precent: {
+                    maszas: 0,
+                    eses: 0,
+                    ugras: 0,
+                    lopakodas: 0,
+                    rejtozes: 0,
+                    koteltanc: 0,
+                    zsebmetszes: 0,
+                    csabdafelfedezes: 0,
+                    zarnyitas: 0,
+                    titkosajto: 0,
                 },
-                {
-                    name: 'Erdőjárás',
-                    level: 'MF',
-                    kp: 0,
-                },
-                {
-                    name: 'Idomitás',
-                    level: 'MF',
-                    kp: 0,
-                }
-            ],
+            },
             description: [
                 'Az elfeket két különböző csoportba oszthatjuk: a közönséges elfekre - akiknek jó néhány alfaja létezik, és az óelfekre. Miután a MAGUS rendszerében csak közönséges elffel lehet játszani, ezért itt és most csak rájuk térünk ki.',
                 'Negyvenezer éve még csak Dél-Zneven léteztek elfek. Származásukat illetően csupán legendáikra hagyatkozhatunk - a legtöbb bölcs Larion szigetét tartja őshazájuknak. A kráni Sötét Hatalom megjelenése után a Délen virágzó elf birodalom hanyatlásnak indult. Életterük egyre szűkült, némely csoportjaik a fenyegetés elől Északra menekültek, mig a délen maradottak beszorultak Tzsson Larba, hogy azután onnan is tovább meneküljenek Lasmosiüfélszigetre. Itt élnek ma is, Elfendelnek nevezett királyságukban. Északra költözött társaik szerencsésebbnek bizonyultak: a Nyugati-óceán partjától a kontinens belsejéig húzódó komor erdőségekben - távol majd minden emberi civilizációtól - telepedtek meg. Birodalmukat Sirenar Szövetség néven jegyzik az ynevi bölcsek.',
@@ -82,18 +96,22 @@
             forbittenClasses: ['PAP', 'PAPLOVAG', 'TUZVARAZSLO'],
             CE: 10,
             specials: ['Másfélszer jobb hallás', 'Kiváló futó', 'Infralátás 10 méterig', 'Nekromancia érzékenység -6'],
-            skills: [
-                {
-                    name: 'Lovaglás',
-                    level: 'MF',
-                    kp: 0,
+            skills: {
+                af: [],
+                mf: ['LOVAGLAS', 'IDOMITAS'],
+                precent: {
+                    maszas: 0,
+                    eses: 0,
+                    ugras: 0,
+                    lopakodas: 0,
+                    rejtozes: 0,
+                    koteltanc: 0,
+                    zsebmetszes: 0,
+                    csabdafelfedezes: 0,
+                    zarnyitas: 0,
+                    titkosajto: 0,
                 },
-                {
-                    name: 'Idomitás',
-                    level: 'MF',
-                    kp: 0,
-                }
-            ],
+            },
             description: [
                 'A félelfek Ynev legkülönösebb jellemű lényei. Egyik szülőjük elf, mig a másik ember, s ez a kettőség egész - igen hosszú - életük során végigkiséri őket. Igazán nem képesek beilleszkedni egyik nép világába sem - az embereknek túlságosan elfek, az elfeknek túlságosan emberek. Gondolkodásmódjukban közelebb állnak az emberekhez - noha mindég egyfajta töprengő, a világ dolgain merengő hozzáállás jellemzi őket -, külsejük azonban magán hordozza elfőseik örökségét. Légiesebbek, könnyedebbek mint az emberek - igaz, az elfeknél erősebb testalkatúak -,vonásaik megnyerőek, hajuk szőke, olykor ezüstszin, szemük legtöbbször ibolyakék. Állandóan nyughatatlanok. Egyszer egy Dorani bölcs azt találta állitani róluk, hogy örökké helyüket keresik a világban, s ez kissé profán általánositás, van benn némi igazság. Igen sok kalandozó kerül ki közülük, ennek oka nyilván a fenti tényekben keresendő. A két faj számtalan kitünő tulajdonságát egyesiti, ám vég nélkül - önmaguk számára is megmafyarázhatatlan - elégedetlenségük olyan átok, mely megakadályozza őket abban, hogy népük döntő befolyásolással birjon a világ sorsának alakitásában. A természet furcsa fintora, hogy két félelf utódja is félelf lesz, sőt egy flelf és egy tisztavérú elf frigyéből is csak félelf születhet. Félelf és ember kapcsolatából mindég ember születik.',
                 'A félelfek is rajongva szeretik a természetet és az élőlényeket, noha korántsem olyan töretlen a kapcsolatuk vele, mint tisztavérű társaiké. Éltalában az Élet és a Rend erkölcsei szerint élik életüket, noha találhatunk köztük velejéig romlott, önmaguktól és a világtól megkeseredett gyilkosokat is. Hazudni hamar megtanultak, ám a barátság szentségét még a legelvetemültebbek sem gyalázzák meg.',
@@ -115,18 +133,22 @@
             forbittenClasses: ['FEJVADASZ', 'LOVAG', 'BARD', 'PAPLOVAG', 'HARCMUVESZ', 'KARDMUVESZ', 'BOSZORKANY', 'BOSZORKANYMESTER', 'TUZVARAZSLO'],
             CE: 0,
             specials: ['Infralátás 30 méter', 'Kiválló időérzék', 'Föld alatt mélységérzett - 2 méter pontosság', '+-5 év pontossággal meghatározza egy épület korát'],
-            skills: [
-                {
-                    name: 'Csabdafelfedezés',
-                    level: '35%',
-                    kp: 0,
+            skills: {
+                af: [],
+                mf: [],
+                precent: {
+                    maszas: 0,
+                    eses: 0,
+                    ugras: 0,
+                    lopakodas: 0,
+                    rejtozes: 0,
+                    koteltanc: 0,
+                    zsebmetszes: 0,
+                    csabdafelfedezes: 35,
+                    zarnyitas: 0,
+                    titkosajto: 30,
                 },
-                {
-                    name: 'Titkosajtó keresés',
-                    level: '30%',
-                    kp: 0,
-                }
-            ],
+            },
             description: [
                 'A törpék Beriquelről érkeztek, a P.sz. 1200-as években. Hamarosan barátságot kötöttek az Északi Szövetség népeivel, akik eleinte ugyan különösnek találták ezt az apró termetű fajt, mely szokásában és gondolkodásában igen elüt az embertől - ám rövidesen rádöbbentek, milyen kemény harcosokra, milyen hű barátokra tettek szert.',
                 'Való igaz: a törpéknél, az épitészet, a statika szerelmeseinél keresve sem találni rendithetetlenebb társakat. Életük egész tartalma alatt az állandóságot a nyugalmat keresik - ám mivel Ynev és a valóság talaján is két lábbal állnak, pontosan felmérik, hogy az állandósághoy, a nyugalomhoz és a kényelemhez munkára, vagyonra, vagyis kiemelkedetségre van szükség.',
@@ -152,18 +174,22 @@
             forbittenClasses: ['LOVAG', 'BARD', 'PAPLOVAG', 'HARCMUVESZ', 'KARDMUVESZ', 'BOSZORKANY', 'TUZVARAZSLO', 'VARAZSLO'],
             CE: 0,
             specials: ['Infralátás 15 méterig', 'Föld alatti mélységérzett - 4 méter pontosság', 'Ötször jobb szaglás'],
-            skills: [
-                {
-                    name: 'Csabdafelfedezés',
-                    level: '20%',
-                    kp: 0,
+            skills: {
+                af: [],
+                mf: [],
+                precent: {
+                    maszas: 0,
+                    eses: 0,
+                    ugras: 0,
+                    lopakodas: 0,
+                    rejtozes: 0,
+                    koteltanc: 0,
+                    zsebmetszes: 0,
+                    csabdafelfedezes: 20,
+                    zarnyitas: 0,
+                    titkosajto: 10,
                 },
-                {
-                    name: 'Titkosajtó keresés',
-                    level: '10%',
-                    kp: 0,
-                }
-            ],
+            },
             description: [
                 'Mágikus beavatkozás eredménye képpen létrejött faj, melynek egyedei az orkok legértékesebb tulajdonságait csillogtatják: a makacsságig menő kitartást, a természetes, állati inteligenciát, a kérhetetlen vadságot - ezúttal az emberiség céljai érdekében.',
                 'Az udvari - másnéven nemesitett - orkok Északon jelentek meg, első osztaguk egy ottani hatalmasság szolgálatában állt. A világhir, azaz inkább a koronás fők divathóbortja akkor kapta szárnyára őket, mikor kemény küzdelemben megmentették a szorongatott nagyúr életét.',
