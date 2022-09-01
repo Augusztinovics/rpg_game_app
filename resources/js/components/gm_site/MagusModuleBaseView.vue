5f2f48d1e1cb7e3502dca8df1ebd66619db2fcb0
Augusztinovics Janos <lancing0@gmail.com>
2022-09-01 05:33:52 +0100

    space

--- resources/js/components/gm_site/MagusModuleBaseView.vue
+++ resources/js/components/gm_site/MagusModuleBaseView.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
         <div class="p-4 border-bottom border-primary m-2 text-center">
-            <button type="button" class="btn btn-success costum-btn">Új játék készítése</button>
+            <button type="button" class="btn btn-success costum-btn">
+                Új játék készítése
+            </button>
         </div>
         <div v-if="havePagination" class="text-center mt-4">
-            <button v-for="pag, index in pagLinks" :key="'PT' + index" :disabled="!pag.url" :class="{'active':pag.active}" class="btn btn-sm" @click="paginate(pag.url)" v-html="pag.label"></button>
+            <button
+                v-for="(pag, index) in pagLinks"
+                :key="'PT' + index"
+                :disabled="!pag.url"
+                :class="{ active: pag.active }"
+                class="btn btn-sm"
+                @click="paginate(pag.url)"
+                v-html="pag.label"
+            ></button>
         </div>
         <div class="table-responsive m-4">
             <table class="table table-striped">
@@ -16,20 +26,47 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="gameModule, index in gameModules" :key="'GM' + index">
+                    <tr
+                        v-for="(gameModule, index) in gameModules"
+                        :key="'GM' + index"
+                    >
                         <td>{{ gameModule.id }}</td>
-                        <td><a href="" class="btn btn-link">{{ gameModule.name }}</a></td>
                         <td>
-                            <a href="" class="btn btn-outline-success m-1">Megnéz</a>
-                            <button type="button" class="btn btn-success costum-btn m-1">Módosít</button>
-                            <button type="button" class="btn btn-danger costum-btn m-1">Töröl</button>
+                            <a href="" class="btn btn-link">{{
+                                gameModule.name
+                            }}</a>
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-outline-success m-1"
+                                >Megnéz</a
+                            >
+                            <button
+                                type="button"
+                                class="btn btn-success costum-btn m-1"
+                            >
+                                Módosít
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-danger costum-btn m-1"
+                            >
+                                Töröl
+                            </button>
                         </td>
                     </tr>
                 </tbody>
             </table>
         </div>
         <div v-if="havePagination" class="text-center">
-            <button v-for="pag, index in pagLinks" :key="'PT' + index" :disabled="!pag.url" :class="{'active':pag.active}" class="btn btn-sm" @click="paginate(pag.url)" v-html="pag.label"></button>
+            <button
+                v-for="(pag, index) in pagLinks"
+                :key="'PT' + index"
+                :disabled="!pag.url"
+                :class="{ active: pag.active }"
+                class="btn btn-sm"
+                @click="paginate(pag.url)"
+                v-html="pag.label"
+            ></button>
         </div>
         Magus
     </div>
@@ -41,7 +78,7 @@ export default {
         return {
             gameModules: [],
             pagLinks: [],
-        }
+        };
     },
     computed: {
         havePagination() {
@@ -49,37 +86,41 @@ export default {
         },
     },
     methods: {
-         fetchGameModules() {
-            axios.get('gm/game-modules/MAGUS')
-            .then(res => {
-                this.gameModules = res.data;
-                //this.gameModules = res.data.data;
-                this.pagLinks = res.data.links;
-                //csak most
-                this.pagLinks = [];
-                //end csak most
-            }).catch(error => {
-                console.log(error);
-            })
+        fetchGameModules() {
+            axios
+                .get("gm/game-modules/MAGUS")
+                .then((res) => {
+                    this.gameModules = res.data;
+                    // this.gameModules = res.data.data;
+                    this.pagLinks = res.data.links;
+                    //csak most
+                    this.pagLinks = [];
+                    //end csak most
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         },
         paginate(link) {
-            axios.get(link)
-            .then(res => {
-                this.gameModules = res.data.data;
-                this.pagLinks = res.data.links;
-            }).catch(error => {
-                console.log(error);
-            })
+            axios
+                .get(link)
+                .then((res) => {
+                    this.gameModules = res.data.data;
+                    this.pagLinks = res.data.links;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         },
     },
     mounted() {
         this.fetchGameModules();
-    }
-}
+    },
+};
 </script>
 
 <style scoped>
-    .active{
-        border: 1px solid rgb(15, 91, 161);
-    }
+.active {
+    border: 1px solid rgb(15, 91, 161);
+}
 </style>
