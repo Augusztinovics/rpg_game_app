6b8b543a1c1d52f6aa7791296ab78c4521f1e382
Augusztinovics Janos <lancing0@gmail.com>
2022-09-23 07:43:49 +0100

    stage base

---
+++ resources/js/components/gm_site/MagusModuleStage.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <!-- Jelenet kontener -->
+        <div>
+            <!-- Jelenet sorszama -->
+            <div class="text-end">
+                <p>{{ module_data.game_module_data_order}}</p>
+            </div>
+            <!-- Jelenet cim -->
+            <div class="text-center m-3">
+                <h1>{{ moduleData.module_data.title }}<span class="ms-3"><button type="button" class="btn btn-success btn-sm costum-btn">Módosít</button></span></h1>
+            </div>
+            <!-- Jelenet leirasa hatterkeppel -->
+            <div class="text-center">
+                <div class="text-end">
+                    <button type="button" class="btn btn-success btn-sm costum-btn">Háttér kép</button>
+                </div>
+                <div>
+                    <p>{{ moduleData.module_data.description }}</p>
+                    <button type="button" class="btn btn-success btn-sm costum-btn">Módosít</button>
+                </div>
+            </div>
+            <!-- Terkep rajzolas -->
+            <div>
+
+            </div>
+        </div>
+
+        <!-- Edit modals -->
+        <!-- CIM -->
+        <!-- HATTERKEP -->
+        <!-- LEIRAS -->
+        <!-- JELENET NPC -->
+        <!-- JELENET JEGYZET -->
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        moduleData: Object
+    },
+    data() {
+        return {
+            inputTitle: '',
+            inputDescript: '',
+        }
+    },
+}
+</script>
