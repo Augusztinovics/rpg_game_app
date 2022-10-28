9c24aabaef38e8ace81b7fd54a91d767493494ff
Augusztinovics Janos <lancing0@gmail.com>
2022-10-28 07:06:01 +0100

    character sheet base

---
+++ resources/views/pdf/maguscharactersheet.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magus karakter lap</title>
+    <style>
+        * {
+            font-family: DejaVu Sans !important;
+        }
+        .page-break {
+            page-break-after: always;
+        }
+        
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
